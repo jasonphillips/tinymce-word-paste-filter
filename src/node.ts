8ecf6aa56f56191cd79140401a8d0dccace9069f
jasonphillips <jason.surname.phillips@gmail.com>
2020-05-25 19:29:22 -0500

    include matchMedia mock for node envs

--- src/node.ts
+++ src/node.ts
@@ -2,9 +2,20 @@
 const noop = (content: string) => content
 let main = noop
 
-if (typeof(navigator)==='object') {
+if (typeof(navigator)==='object' && typeof(window)==='object') {
+
+  // include this mock if missing in env
+  if (typeof(window.matchMedia)==='undefined') {
+    Object.defineProperty(window, 'matchMedia', {
+      value: () => ({
+        matches: false,
+        addListener: () => {},
+        removeListener: () => {}
+      })
+    })
+  }
+
   main = require('./index').default
 }
 
 export default main
-
