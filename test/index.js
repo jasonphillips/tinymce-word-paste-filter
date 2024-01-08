85f689a4f2ab2d2fb68b4b4c74c46a13be353b4b
Shayan Sulehri <shayan.sulehri@gmail.com>
2024-01-08 01:35:50 +0000

    Making matchMedia mock configureable

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const {JSDOM} = require('jsdom');
+const { JSDOM } = require('jsdom');
 
 const wordInput = require('./fixtures/wordInput');
 const cleanedOutput = require('./fixtures/cleanedOutput');
@@ -7,42 +7,43 @@ const complexInput = require('./fixtures/complexInput');
 const complexCleanedOutput = require('./fixtures/complexCleanedOutput');
 
 const dom = new JSDOM();
-const {document, navigator, URL} = dom.window;
+const { document, navigator, URL } = dom.window;
 global.document = document;
 global.navigator = navigator;
 global.URL = URL;
 global.window = dom.window;
 
 Object.defineProperty(window, 'matchMedia', {
-	writable: true,
-	value: jest.fn().mockImplementation((query) => ({
-	    matches: false,
-	    media: query,
-	    onchange: null,
-	    addListener: jest.fn(), // deprecated
-	    removeListener: jest.fn(), // deprecated
-	    addEventListener: jest.fn(),
-	    removeEventListener: jest.fn(),
-	    dispatchEvent: jest.fn(),
-	  }))
-})
+  writable: true,
+  configurable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
 
 const filterWord = require('../').default;
 
 describe('fitlers MS Word content', function () {
-    it('transforms lists; cleans up styles', function () {
-        const result = filterWord(wordInput);
-        assert.equal(result, cleanedOutput);
-    });
+  it('transforms lists; cleans up styles', function () {
+    const result = filterWord(wordInput);
+    assert.equal(result, cleanedOutput);
+  });
 
-    it('leaves non-Word content untouched', function () {
-        const content = `<p style="font-weight:bold">hello world</p>`;
-        const result = filterWord(content);
-        assert.equal(result, content);
-    });
+  it('leaves non-Word content untouched', function () {
+    const content = `<p style="font-weight:bold">hello world</p>`;
+    const result = filterWord(content);
+    assert.equal(result, content);
+  });
 
-    it('handles a complex case', function () {
-        const result = filterWord(complexInput);
-        assert.equal(result, complexCleanedOutput);
-    });
+  it('handles a complex case', function () {
+    const result = filterWord(complexInput);
+    assert.equal(result, complexCleanedOutput);
+  });
 });
